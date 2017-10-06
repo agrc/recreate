375e149c1afb4104ea7b2ee1bafbbe73881be028
Scott Davis <stdavis@utah.gov>
2017-10-06 09:34:55 -0600

    main app layout

--- src/App.js
+++ src/App.js
@@ -1,18 +1,16 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+      <div className="app">
+        <header className="header">
+          <a href='/'>Recreation.Utah.Gov</a>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div className="main">
+
+        </div>
       </div>
     );
   }
