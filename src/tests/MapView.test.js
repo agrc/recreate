e15cea89c87d112a9c034b344d1d647db81a13d1
Scott Davis <stdavis@utah.gov>
2017-10-12 15:14:33 -0600

    fix tests

--- src/tests/MapView.test.js
+++ src/tests/MapView.test.js
@@ -2,6 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import MapView from '../MapView';
 import { MemoryRouter } from 'react-router';
+import geolocate from 'mock-geolocation';
+
+
+beforeAll(() => {
+  geolocate.use();
+  geolocate.send({lng: -111, lat: 49});
+});
+afterAll(() => {
+  geolocate.restore();
+});
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
