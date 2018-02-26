158739f7c6567255112f059228e2926ef8dca939
Scott Davis <stdavis@utah.gov>
2018-02-26 11:03:06 -0700

    v0.6.0 changelog and version bump

--- src/ChangeLog.js
+++ src/ChangeLog.js
@@ -8,6 +8,13 @@ class ChangeLog extends Component {
       <div className='changelog'>
         <Button size='sm' color='primary' onClick={() => this.props.history.goBack()}>Back</Button>
         <h5>Change Log</h5>
+        <strong>v0.6.0</strong>
+        <ul>
+          <li>Fix styling of list table (<a href='https://github.com/agrc/recreate-web/issues/30'>#30</a>)</li>
+          <li>Styling improvements for home page including switching text in GOED logo from black to white.</li>
+          <li>Remove Mapzen elevation profile service in favor of precached values (<a href='https://github.com/agrc/recreate-web/issues/25'>#25</a>)</li>
+          <li>Better hiking details layout and add elevation gain (<a href='https://github.com/agrc/recreate-web/issues/2'>#2</a>)</li>
+        </ul>
         <strong>v0.5.0</strong>
         <ul>
           <li>Add change log ;) (<a href='https://github.com/agrc/recreate-web/issues/18'>#18</a>)</li>
