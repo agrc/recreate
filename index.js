571f413b60fb17b25fccb7e9b3bf01939b89d70d
Scott Davis <stdavis@utah.gov>
2018-03-26 12:54:08 -0600

    Add RouterWrapper
    This allows App to have access to the router history for the back button.

--- index.js
+++ index.js
@@ -1,4 +1,20 @@
+import React from 'react';
 import { AppRegistry } from 'react-native';
 import App from './src/App';
+import { NativeRouter as Router, BackButton } from 'react-router-native';
 
-AppRegistry.registerComponent('RecreateNative', () => App);
+
+const initialEntries = ['/feature/{46C93DBE-8C45-4CDA-AC74-0E78BAF91A25}'];  // FeatureDetails
+// const initialEntries = undefined;  // home
+
+const RouterWrapper = function (props) {
+  return (
+    <Router basename='/' initialEntries={initialEntries}>
+      <BackButton>
+        <App />
+      </BackButton>
+    </Router>
+  );
+};
+
+AppRegistry.registerComponent('RecreateNative', () => RouterWrapper);
--- src/App.js
+++ src/App.js
@@ -1,44 +1,58 @@
 import React, { Component } from 'react';
 
-import { Container, Header, Body, StyleProvider } from 'native-base';
+import { Container, Header, Body, Left, Button, StyleProvider, Icon, Right } from 'native-base';
 import getTheme from './native-base-theme/components';
 import platform from './native-base-theme/variables/platform';
-import DefaultText from './DefaultText';
-import { NativeRouter as Router, Route, Link, Switch } from 'react-router-native';
+import { WhiteText } from './AppText';
+import { Route, Link, Switch, withRouter } from 'react-router-native';
 import Home from './Home';
 import MapView from './MapView';
 import MapboxGL from '@mapbox/react-native-mapbox-gl';
 import { REACT_APP_MAPBOX_TOKEN } from 'react-native-dotenv';
 
-// import FeatureDetails from './FeatureDetails';
+import FeatureDetails from './FeatureDetails';
 // import DetailMap from './DetailMap';
 // import ChangeLog from './ChangeLog';
 
 
 MapboxGL.setAccessToken(REACT_APP_MAPBOX_TOKEN);
-export default class App extends Component {
+class App extends Component {
+  goBack() {
+    console.log(this.props.history);
+
+    this.props.history.goBack();
+  }
+
   render() {
     return (
-      <Router basename='/' >
-        <StyleProvider style={getTheme(platform)}>
-          <Container>
-            <Header>
-              <Body>
-                <Link to='/'><DefaultText>Recreate Utah</DefaultText></Link>
-              </Body>
-            </Header>
-            <Switch>
-              <Route exact={true} path='/' component={Home} />
-              <Route path='/map/:location?/:list?' component={MapView} />
-              {/*
-              <Route exact={true} path='/feature/:id' component={FeatureDetails} />
-              <Route path='/feature/:id/map' component={DetailMap} />
-              <Route path='/changelog' component={ChangeLog} />
-              */}
-            </Switch>
-          </Container>
-        </StyleProvider>
-      </Router>
+      <StyleProvider style={getTheme(platform)}>
+        <Container>
+          <Header>
+            <Left>
+              { this.props.history.index > 0 &&
+                <Button transparent onPress={this.goBack.bind(this)}>
+                  <Icon name='arrow-back' style={{color: 'white'}} />
+                </Button>
+              }
+            </Left>
+            <Body>
+              <Link to='/'><WhiteText>Recreate Utah</WhiteText></Link>
+            </Body>
+            <Right />
+          </Header>
+          <Switch>
+            <Route exact={true} path='/' component={Home} />
+            <Route path='/map/:location?/:list?' component={MapView} />
+            <Route exact={true} path='/feature/:id' component={FeatureDetails} />
+            {/*
+            <Route path='/feature/:id/map' component={DetailMap} />
+            <Route path='/changelog' component={ChangeLog} />
+            */}
+          </Switch>
+        </Container>
+      </StyleProvider>
     );
   }
 }
+
+export default withRouter(App);
