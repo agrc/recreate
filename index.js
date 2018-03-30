e8cac299ddcaa49f338a5b0829727907bb10f9b1
Scott Davis <stdavis@utah.gov>
2018-03-30 16:55:09 -0600

    additional testing initial paths

--- index.js
+++ index.js
@@ -4,12 +4,13 @@ import App from './src/App';
 import { MemoryRouter as Router, BackButton } from 'react-router-native';
 
 
-// const initialEntries = ['/feature/{46C93DBE-8C45-4CDA-AC74-0E78BAF91A25}'];  // FeatureDetails - Hiking
+// const initialEntries = ['/feature/{63F08536-EB47-4F03-99D5-CA3E6D964C57}'];  // FeatureDetails - Hiking
 // const initialEntries = ['/feature/{1A892CE3-0D34-408B-BB2B-B926D02DC799}'];  // FeatureDetails - Park
 // const initialEntries = ['/feature/{9E41D0F2-E5B5-4C99-ABEF-51442177E035}'];  // FeatureDetails - Boat Ramp
+// const initialEntries = ['/feature/{46C93DBE-8C45-4CDA-AC74-0E78BAF91A25}/map'];  // DetailMap
 // const initialEntries = ['/map/-111.82,40.37,10'];  // map
 
-const initialEntries = undefined;  // home
+const initialEntries = undefined;  // home (Release)
 
 const RouterWrapper = function (props) {
   return (
