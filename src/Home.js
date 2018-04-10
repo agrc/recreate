7cb3c3c85f668157a9b44e39688e09ffc7de42a3
Scott Davis <stdavis@utah.gov>
2018-04-10 09:01:42 -0600

    Implement search box in Home

--- src/Home.js
+++ src/Home.js
@@ -1,13 +1,15 @@
 import React, { Component } from 'react';
-import { Button, Container, Text } from 'native-base';
-import { ImageBackground, StyleSheet, Image, View } from 'react-native';
+import { Button, Container, Input, Item, Text } from 'native-base';
+import { Dimensions, ImageBackground, StyleSheet, Image, View } from 'react-native';
 import { Link } from 'react-router-native';
 import queryString from 'query-string';
-// import mapboxgl from 'mapbox-gl';
 import { version } from '../package.json';
 import config from './config';
 import { WhiteText } from './AppText';
 import LinkButton from './LinkButton';
+import Collapsible from 'react-native-collapsible';
+import geoViewport from '@mapbox/geo-viewport';
+import { REACT_APP_AGRC_WEB_API_KEY } from 'react-native-dotenv';
 
 
 const searchUrl = 'https://api.mapserv.utah.gov/api/v1/search/SGID10.Location.ZoomLocations/Name,shape@envelope';
@@ -25,24 +27,30 @@ export default class Home extends Component {
     this.toggleSearchForm = this.toggleSearchForm.bind(this);
     this.search = this.search.bind(this);
     this.handleCityPlaceChange = this.handleCityPlaceChange.bind(this);
-    this.handleSearchKeyDown = this.handleSearchKeyDown.bind(this);
   }
+
   toggleSearchForm() {
     this.setState({searchFormOpen: !this.state.searchFormOpen});
   }
+
   async search() {
+    if (this.state.cityPlace.length === 0) {
+      return;
+    }
+
     const params = {
         predicate: `Name = '${this.state.cityPlace}'`,
         spatialReference: 4326,
-        apiKey: process.env.REACT_APP_AGRC_WEB_API_KEY
+        apiKey: REACT_APP_AGRC_WEB_API_KEY
     };
 
-    const response = await fetch(`${searchUrl}?${queryString.stringify(params)}`);
+    const headers = { Referer: 'https://recreate.utah.gov/' };
+    const response = await fetch(`${searchUrl}?${queryString.stringify(params)}`, { headers });
     if (response.ok) {
       const responseJson = await response.json();
       if (responseJson.result.length) {
-        const extent = this.getMapboxCentroid(responseJson.result[0].geometry);
-        this.props.history.push(`/map/${extent},11`);
+        const {center, zoom} = this.getViewport(responseJson.result[0].geometry);
+        this.props.history.push(`/map/${center},${zoom}`);
       } else {
         this.setState({ searchError: true });
       }
@@ -50,33 +58,34 @@ export default class Home extends Component {
       this.setState({ searchError: true });
     }
   }
-  // getMapboxCentroid(esriGeometry) {
-  //   // returns a LngLat array that is the centroid of `esriGeometry`
-  //   let southwest;
-  //   let northeast;
-  //
-  //   esriGeometry.rings[0].forEach((point) => {
-  //     const diviser = 1000;
-  //     point = point.map((coord) => Math.round(coord * diviser)/diviser)
-  //     if (!southwest || (point[0] < southwest[0] && point[1] < southwest[1])) {
-  //       southwest = point;
-  //     } else if (!northeast || (point[0] > northeast[0] && point[1] > northeast[1])) {
-  //       northeast = point;
-  //     }
-  //   });
-  //
-  //   return new mapboxgl.LngLatBounds(southwest, northeast).getCenter().toArray();
-  // }
-  handleCityPlaceChange(event) {
-    this.setState({cityPlace: event.target.value});
-  }
-  handleSearchKeyDown(event) {
-    this.setState({searchError: false});
 
-    if (event.key === config.enterKey) {
-      this.search();
-    }
+  getViewport(esriGeometry) {
+    let southwest;
+    let northeast;
+
+    esriGeometry.rings[0].forEach((point) => {
+      const diviser = 1000;
+      point = point.map((coord) => Math.round(coord * diviser)/diviser)
+      if (!southwest || (point[0] < southwest[0] && point[1] < southwest[1])) {
+        southwest = point;
+      } else if (!northeast || (point[0] > northeast[0] && point[1] > northeast[1])) {
+        northeast = point;
+      }
+    });
+
+    const {height, width} = Dimensions.get('window');
+
+    return geoViewport.viewport([southwest[0], southwest[1], northeast[0], northeast[1]],
+                                [width, height],
+                                undefined,
+                                undefined,
+                                config.tileSize);
   }
+
+  handleCityPlaceChange(searchTerm) {
+    this.setState({ cityPlace: searchTerm, searchError: false });
+  }
+
   render() {
     return (
       <Container>
@@ -90,21 +99,25 @@ export default class Home extends Component {
               <LinkButton primary block style={{marginBottom: padding}} to='/map'>
                 <Text>Explore Current Location</Text>
               </LinkButton>
-              <Button primary block onClick={this.toggleSearchForm}>
+              <Button primary block onPress={this.toggleSearchForm}>
                 <Text>Search by City or Place</Text>
               </Button>
-              {/*
-              <Collapse isOpen={this.state.searchFormOpen} className='search-form'>
-                <Input type='text' placeholder='Enter City or Place' value={this.state.cityPlace}
-                  onChange={this.handleCityPlaceChange}
-                  onKeyDown={this.handleSearchKeyDown}/>
-                <div className='button-group'>
-                  <Button color='primary' onClick={this.search}>Search</Button>
-                  <Button color='warning' onClick={() => this.setState({searchFormOpen: false})}>Cancel</Button>
-                </div>
-                <span style={{display: (this.state.searchError) ? 'block': 'none'}}>No results found for {this.state.cityPlace}!</span>
-              </Collapse>
-              */}
+              <Collapsible collapsed={!this.state.searchFormOpen}>
+                <Item regular style={styles.textbox}>
+                  <Input placeholder='Enter City or Place'
+                    value={this.state.cityPlace}
+                    onChangeText={this.handleCityPlaceChange} />
+                </Item>
+                <View style={styles.searchButtonsContainer}>
+                  <Button primary onPress={this.search} style={styles.searchButton}>
+                    <Text>Search</Text>
+                  </Button>
+                  <Button warning onPress={() => this.setState({searchFormOpen: false})} style={styles.searchButton}>
+                    <Text>Cancel</Text>
+                  </Button>
+                </View>
+                <WhiteText style={(this.state.searchError) ? null : styles.hidden}>No results found for {this.state.cityPlace}!</WhiteText>
+              </Collapsible>
             </View>
             <Image source={require('./images/outdoorlogo.png')} style={styles.goedLogo}/>
             <Link to='changelog' style={styles.version}>
@@ -135,6 +148,16 @@ const styles = StyleSheet.create({
   goedLogo: {
     alignSelf: 'center'
   },
+  hidden: {
+    display: 'none'
+  },
+  searchButton: {
+    width: '25%'
+  },
+  searchButtonsContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between'
+  },
   tagLineContainer: {
     alignSelf: 'center'
   },
@@ -147,6 +170,15 @@ const styles = StyleSheet.create({
     fontSize: 50,
     fontWeight: 'bold',
     textAlign: 'center'
+  },
+  textbox: {
+    borderColor: config.colors.transparent,
+    backgroundColor: config.colors.white,
+    borderRadius: 5,
+    marginTop: 10,
+    marginBottom: 10,
+    borderWidth: 0,
+    marginLeft: -1
   },
   version: {
     position: 'absolute',
